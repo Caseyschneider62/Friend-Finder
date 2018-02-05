52eecd4217b534aadc101ed3ba91a475cf3797dd
casey schneider <caseyschneider2018@caseys-MBP.attlocal.net>
2018-02-05 13:23:01 -0800

    friend finder

--- server.js
+++ server.js
@@ -16,21 +16,38 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(express.static(__dirname + '/app/public'));
 
-// Express app handle data parsing
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json());
 
+
+
+// app.get("/", function(req, res) {
+// 	res.sendFile(path.join(__dirname, "/../public/view.html"));
+// });
+
+// app.get("/survey", function(req, res) {
+// 	res.sendFile(path.join(__dirname, "/../public/survey.html"));
+// });
+
+// app.post("/api/new", function(req, res) {
+// 	 var newcharacter = req.body;
+//   console.log(newcharacter);
+//   characters.push(newcharacter);
+//   res.json(newcharacter);
+// });
 
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when 
 // users visit or request data from various URLs.
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 // LISTENER
 // The below code effectively "starts" our server
-app.listen(PORT, '0.0.0.0');
-// app.listen(PORT, function() {
-// 	console.log("App listening on PORT: " + PORT);
-// });
+// app.listen(PORT, '0.0.0.0');
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
+
+
+
+
