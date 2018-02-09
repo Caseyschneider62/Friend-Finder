a15bd8b9dba2199ecbfe622acb61fca9e6b7150c
casey schneider <caseyschneider2018@caseys-MacBook-Pro.local>
2018-02-08 17:59:02 -0800

    Final finder

--- app/data/friends.js
+++ app/data/friends.js
@@ -17,13 +17,3 @@ module.exports = [{"name":"Danger","photo":"https://sp.yimg.com/ib/th?id=OIP.0k2
 
 
 
-
-// module.exports = [{"name":"George","photo":"http://dpshots.com/images/every_face_has_a_story/irish-old-man.jpg","scores":["3","3","3","3","3","3","3","3","3","3"]}];
-// module.exports = [{"name":"Clownman","photo":"http://dpshots.com/images/every_face_has_a_story/showman.jpg","answers":["2","2","2","2","2","2","2","2","2","2"]}];
-// module.exports = [{"name":"Clara","photo":"https://sp.yimg.com/ib/th?id=OIP.xWnnNdLTF7zZogjbA_j-SgHaHW&pid=15.1&rs=1&c=1&qlt=95&w=107&h=106","answers":["5","5","5","5","5","5","5","5","5","5"]}];
-// module.exports = [{"name":"Marge","photo":"https://sp.yimg.com/ib/th?id=OIP.OuR3ifUrq_03RdlmdzBxMQHaFZ&pid=15.1&rs=1&c=1&qlt=95&w=167&h=121","answers":["4","4","4","4","4","4","4","4","4","4"]}];
-// module.exports = [{"name":"2face","photo":"https://us.123rf.com/450wm/andreypopov/andreypopov1505/andreypopov150500342/39653549-portrait-of-businessman-with-two-face-expression.jpg?ver=6","answers":["2","3","5","5","4","3","3","2","2","1"]}];
-// module.exports = [{"name":"Mike","photo":"https://sp.yimg.com/ib/th?id=OIP.XrqbaQVwvYQ4GG9o82v_rgHaHi&pid=15.1&rs=1&c=1&qlt=95&w=105&h=106","answers":["3","2","4","3","3","5","5","1","2","4"]}];
-// module.exports = [{"name":"Molly","photo":"https://sp.yimg.com/ib/th?id=OIP.hhckgyWw-fmBGJK1sOJLdAEsDb&pid=15.1&rs=1&c=1&qlt=95&w=162&h=118","answers":["4","4","5","3","2","5","4","1","3","1"]}];
-// module.exports = [{"name":"Amy","photo":"https://sp.yimg.com/ib/th?id=OIP.MaKJBDzOPY9pJKaMCGtEwAHaHa&pid=15.1&rs=1&c=1&qlt=95&w=121&h=121","answers":["1","3","5","5","3","2","3","3","4","2"]}];
-// module.exports = [{"name":"Widget","photo":"https://sp.yimg.com/ib/th?id=OIP.Km6TtCoqk654qs0uc7A5_AHaJq&pid=15.1&rs=1&c=1&qlt=95&w=91&h=118","answers":["5","2","1","5","4","5","1","3","3","4"]}];
